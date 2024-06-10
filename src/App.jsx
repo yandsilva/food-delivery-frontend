cf43d502ade50475e6512f2c3845677ee13bc992
YanDSilva <yandsilva94@gmail.com>
2024-06-10 11:51:02 -0300

    construindo navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import "./NavBar.css";
+import { assets } from "../../assets/assets";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
