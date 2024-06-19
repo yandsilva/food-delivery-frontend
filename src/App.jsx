dce5d3a36b58c9a01e61eadb337c0dec5e872249
YanDSilva <yandsilva94@gmail.com>
2024-06-19 12:44:45 -0300

    finalizado o foodDisplay e o footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
 import PalceOrder from "./pages/PlaceOrder/PalceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PalceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PalceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
